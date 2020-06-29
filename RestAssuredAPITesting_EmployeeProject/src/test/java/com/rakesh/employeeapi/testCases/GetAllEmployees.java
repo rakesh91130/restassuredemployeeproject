8ec7ae44ce03efc0e6f628f2b58a7bb1661017b1
RAKESH KUMAR <rkkumar1992a5@gmail.com>
2020-06-29 09:49:56 +0530

    Commit to test emailTriggering on Push to remote repository

--- RestAssuredAPITesting_EmployeeProject/src/test/java/com/rakesh/employeeapi/testCases/GetAllEmployees.java
+++ RestAssuredAPITesting_EmployeeProject/src/test/java/com/rakesh/employeeapi/testCases/GetAllEmployees.java
@@ -9,26 +9,24 @@ import com.rakesh.employeeapi.base.TestBase;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 
-public class GetAllEmployees extends TestBase{
-	
+public class GetAllEmployees extends TestBase {
+
 	@BeforeClass
-	void getAllEmployees() throws InterruptedException{
+	void getAllEmployees() throws InterruptedException {
 		logger.info("*********Started getAllEployees********");
-		RestAssured.baseURI="http://dummy.restapiexample.com/api/v1";
-		httpRequest=RestAssured.given();
-		response=httpRequest.request(Method.GET,"/employees");
-		
+		RestAssured.baseURI = "http://dummy.restapiexample.com/api/v1";
+		httpRequest = RestAssured.given();
+		response = httpRequest.request(Method.GET, "/employees");
+
 		Thread.sleep(3);
 	}
-	
+
 	@Test
 	void checkingResponseBody() {
 		logger.info("*********checking Response Body********");
-		String responseBody=response.getBody().asString();
-		logger.info("Response body==> "+responseBody);
-		Assert.assertTrue(responseBody!=null);
+		String responseBody = response.getBody().asString();
+		logger.info("Response body==> " + responseBody);
+		Assert.assertTrue(responseBody != null);
 	}
 
-	
 }
-
